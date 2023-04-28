79bcd3c056231ba4e6b8247767324b0de3d88604
YuviPanda <yuvipanda@gmail.com>
2023-04-27 18:03:19 -0700

    Initial commit

---
+++ kubespawner_dynamic_building_ui/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+
+HERE = os.path.dirname(__file__)
+
+TEMPLATE_PATHS = [os.path.join(HERE, 'templates')]
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='jupyterhub-kubespawner-dynamic-building-ui',
+    version='1.0.0',
+    description='UI that plugs into kubespawner profile list for',
+    url='https://github.com/jupyterhub/tmpauthenticator',
+    author='Yuvi Panda',
+    author_email='yuvipanda@gmail.com',
+    license='3 Clause BSD',
+    packages=find_packages(),
+)
