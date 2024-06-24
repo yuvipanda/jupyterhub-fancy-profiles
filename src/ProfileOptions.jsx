20a97bcdbdfeebb11b762440b0e8a09489d0640a
Sanjay Bhangar <sanjay@developmentseed.org>
2024-06-24 14:32:14 +0530

    lintfix, comment out more things

--- src/ProfileOptions.jsx
+++ src/ProfileOptions.jsx
@@ -1,22 +1,22 @@
 import ResourceSelect from "./ResourceSelect";
-import { ImageBuilder } from "./ImageBuilder";
+// import { ImageBuilder } from "./ImageBuilder";
 
 export function ProfileOptions({ config, profile }) {
   return (
     <div className="form-grid">
       {Object.entries(config).map(([key, option]) => {
-        const customOptions =
-          key === "image"
-            ? [
-                {
-                  value: "--extra-selectable-item",
-                  label: "Build your own image",
-                  description:
-                    "Use a mybinder.org compatible GitHub repo to build your own image",
-                  component: ImageBuilder,
-                },
-              ]
-            : [];
+        // const customOptions =
+        //   key === "image"
+        //     ? [
+        //         {
+        //           value: "--extra-selectable-item",
+        //           label: "Build your own image",
+        //           description:
+        //             "Use a mybinder.org compatible GitHub repo to build your own image",
+        //           component: ImageBuilder,
+        //         },
+        //       ]
+        //     : [];
 
         return (
           <ResourceSelect
